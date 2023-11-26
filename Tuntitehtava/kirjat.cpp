0c7b6ddcfc3a67841e2565da4c35d7a5afb04c92
Kasim Zegina <122457305+ChocoWOW@users.noreply.github.com>
2023-11-27 01:00:34 +0200

    Update kirjat.cpp

--- Tuntitehtava/kirjat.cpp
+++ Tuntitehtava/kirjat.cpp
@@ -2,30 +2,79 @@
 #include <string>
 
 using namespace std;
+
 class Julkaisu
 {
-    private:
-
-    string _nimi;
-
-    public:
-        Julkaisu(const string& nimi) : _nimi(nimi)
-        {}
-        virtual void tulosta_tiedot() const
-        {
-            cout << "Julkaisun nimi: " << _nimi << endl;
-        }
-
-};
-class Kirja: public Julkaisu
-{
-private:
-    string _kirjoittaja;
-    int _sivut;
-
 public:
+    string nimi;
+    Julkaisu(string _nimi) : nimi(_nimi)
+    {
+        cout << "Julkaisun konstruktori\n";
+    }
+    virtual ~Julkaisu(){
+        cout << "Julkaisun destruktori\n";
+    }
+    virtual void tulosta_tiedot() = 0;
 
-    Kirja(const string& nimi, const string& kirjoittaja, int sivut):
-        Julkaisu(nimi), _kirjoittaja(kirjoittaja), _sivut(sivut)
-    {}
 };
+class Kirja : public Julkaisu
+{
+public:
+    int sivumaara;
+    string kirjoittaja;
+    Kirja(int _sivumaara, string _kirjoittaja, string nimi) 
+    : Julkaisu(nimi), sivumaara(_sivumaara),kirjoittaja(_kirjoittaja) 
+    {
+        cout << "Kirjan konstruktori\n";
+    }
+
+    
+    ~Kirja()
+    {
+        cout << "Kirjan destruktori\n";
+    }
+
+    void tulosta_tiedot()
+    {
+        cout << "Julkaisija: " << nimi << "Kirjoittaja: " << kirjoittaja << "sivumaara: " << sivumaara << "\n";
+    }
+
+
+
+};
+class Lehti : public Julkaisu
+{
+public:
+    string paatoimittaja;
+    Lehti(string _paatoimittaja, string nimi) 
+    : Julkaisu(nimi), paatoimittaja(_paatoimittaja)
+    {
+        cout << "Lehden konstruktori\n";
+    }
+
+    ~Lehti()
+    {
+        cout << "Lehden destruktori\n";
+    }
+    void tulosta_tiedot()
+    {
+        cout << "Julkaisija: " << nimi << "Paatoimittaja: " << paatoimittaja << "\n";
+    }
+};
+
+int main()
+{
+    Julkaisu* kJulkaisu;
+    kJulkaisu = new Kirja(100 , "Rosa Liksom" ,"Hytti n:o 6");
+
+    kJulkaisu->tulosta_tiedot();
+
+    delete kJulkaisu;
+
+    Julkaisu* LeJulkaisu;
+    LeJulkaisu = new Lehti("Aku ankka" ,"aki hyyppa");
+
+    LeJulkaisu->tulosta_tiedot();
+
+    delete LeJulkaisu;
+}
